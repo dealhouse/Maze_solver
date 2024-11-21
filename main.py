b61a3cfd27b58b0ef94574ffd6ad67b38eb3f773
dealhouse <de.juan1@live.com>
2024-11-21 17:25:18 -0400

    added maze class

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ from window import Window
 from line import Line
 from point import Point
 from cell import Cell
+from maze import Maze
 
 win = Window(1600, 1200)
 
-cell1 = Cell(100, 200, 100, 200, True, True, True, True, win)
-cell2 = Cell(300, 400, 300, 400, False, True, True, True, win)
+# cell1 = Cell(100, 200, 100, 200, True, True, True, True, win)
+# cell2 = Cell(300, 400, 300, 400, False, True, True, True, win)
 # cell3 = Cell(500, 600, 500, 600, True, False, True, True, win)
 # cell4 = Cell(700, 800, 700, 800, True, True, False, True, win)
-cell1.draw(100, 200, 100, 200)
-cell2.draw(300, 400, 300, 400)
+# cell1.draw(100, 200, 100, 200)
+# cell2.draw(300, 400, 300, 400)
 # cell3.draw(500, 600, 500, 600)
 # cell4.draw(700, 800, 700, 800)
-cell1.draw_move(cell2)
+# cell1.draw_move(cell2)
+
+maze = Maze(100, 100, 10, 10, 100, 100, win)
 win.wait_for_close()
 
 
---
+++ maze.py
@@ -0,0 +1,38 @@
+from cell import Cell
+import time
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._create_cells()
+
+    def _create_cells(self):
+        self.cells = []
+        for i in range(self.num_rows):
+            column = []
+            for j in range(self.num_cols):
+                x1 = self.x1 + j * self.cell_size_x
+                x2 = self.x1 + (j + 1) * self.cell_size_x
+                y1 = self.y1 + i * self.cell_size_y
+                y2 = self.y1 + (i + 1) * self.cell_size_y
+                cell = Cell(x1, x2, y1, y2, True, True, True, True, self.win)
+                column.append(cell)
+            self.cells.append(column)
+        
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                self._draw_cell(i, j)
+    def _draw_cell(self, i, j):
+        cell = self.cells[i][j]
+        cell.draw()
+        self._animate()
+    
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
+        
