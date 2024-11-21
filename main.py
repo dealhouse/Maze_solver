a2699e4f159e2af1e59f9f8509420576e5b4ac84
dealhouse <de.juan1@live.com>
2024-11-21 15:33:35 -0400

    Added point and line class, and tested them

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from window import Window
+from line import Line
+from point import Point
 
 win = Window(800, 600)
+
+line = Line(Point(100, 100), Point(300, 300))
+line2 = Line(Point(300, 100), Point(100, 300))
+win.draw_line(line, "red")
+win.draw_line(line2, "blue")
 win.wait_for_close()
+
 
 
